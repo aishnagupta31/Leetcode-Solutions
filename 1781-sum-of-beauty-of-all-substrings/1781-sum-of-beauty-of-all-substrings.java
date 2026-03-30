9e0cf5be1ccb17b450f16a0dc93b22cf3155c39a
aishnagupta31 <aishnagupta31@gmail.com>
2026-03-30 11:53:22 -0700

    Time: 60 ms (75.62%), Space: 44 MB (89.08%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.java
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.java
@@ -1,41 +1,26 @@
-import java.util.*;
-
 class Solution {
-    
-    public int calcMin(ArrayList<Integer> arr) {
-        int min = Integer.MAX_VALUE;
-        for (int i = 0; i < arr.size(); i++) {
-            min = Math.min(min, arr.get(i));
-        }
-        return min;
-    }
-
-    public int calcMax(ArrayList<Integer> arr) {
-        int max = Integer.MIN_VALUE;
-        for (int i = 0; i < arr.size(); i++) {
-            max = Math.max(max, arr.get(i));
-        }
-        return max;
-    }
-
     public int beautySum(String s) {
         int n = s.length();
         int sum = 0;
 
         for (int i = 0; i < n; i++) {
-            HashMap<Character, Integer> map = new HashMap<>();
+            int[] freq = new int[26];
 
             for (int j = i; j < n; j++) {
                 char ch = s.charAt(j);
-                map.put(ch, map.getOrDefault(ch, 0) + 1);
+                freq[ch - 'a']++;
 
-                if (map.size() > 1) {
-                    ArrayList<Integer> arr = new ArrayList<>(map.values());
-                    int min = calcMin(arr);
-                    int max = calcMax(arr);
+                int min = Integer.MAX_VALUE;
+                int max = Integer.MIN_VALUE;
 
-                    sum += (max - min);
+                for (int k = 0; k < 26; k++) {
+                    if (freq[k] > 0) {
+                        min = Math.min(min, freq[k]);
+                        max = Math.max(max, freq[k]);
+                    }
                 }
+
+                sum += (max - min);
             }
         }
 
