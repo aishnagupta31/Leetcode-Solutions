bb6af27a0bf9f6580e77a5ccfd929e17dc143324
aishnagupta31 <aishnagupta31@gmail.com>
2026-03-30 23:56:19 -0700

    Time: 23 ms (93.95%), Space: 139.9 MB (40.69%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        int n = nums.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1); // sum and index
+        int sum = 0;
+
+
+        for(int i=0; i<n; i++){
+            sum += nums[i];
+            int rem = (k == 0) ? sum : sum % k;
+
+            if(map.containsKey(rem)){
+                if(i - map.get(rem) >= 2){
+                    return true;
+                }
+            }
+            else{
+                map.put(rem, i);
+            } 
+        }
+        return false;
+    }
+}
