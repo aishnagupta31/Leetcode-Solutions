92d68859447a3ef8c378438bfccea439d89cbd32
aishnagupta31 <aishnagupta31@gmail.com>
2026-04-06 14:00:51 -0700

    Time: 6 ms (15.84%), Space: 46.1 MB (80.38%) - LeetHub

---
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int longestValidParentheses(String s) {
+        int n = s.length();
+        int index = -1;
+        Stack<Integer> stack = new Stack<>();
+        int ans = 0;
+
+        for(int i=0; i<n; i++){
+            if(s.charAt(i) =='('){
+                stack.push(i);
+            }
+            else {
+                if(!stack.isEmpty()){
+                    stack.pop();
+                    if(!stack.isEmpty()){
+                        ans = Math.max(ans, i-stack.peek());
+                    }else{
+                        ans = Math.max(ans,i-index);
+                    }
+                }else{
+                    index = i;
+                }
+            }
+        }
+        return ans;
+    }
+}
+
+
+// https://chatgpt.com/c/69d41d3e-f380-83a8-a875-dc28b37a9282
