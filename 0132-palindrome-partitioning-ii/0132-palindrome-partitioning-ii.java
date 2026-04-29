fc3e0b2f0b07685c7ba97cc7ba8ffbf016f8091b
aishnagupta31 <aishnagupta31@gmail.com>
2026-04-29 12:28:43 -0700

    Time: 2494 ms (15.18%), Space: 42.6 MB (88.88%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
@@ -1,6 +1,6 @@
 class Solution {
-    
-    // Check if substring s[i...j] is palindrome
+
+    // Same palindrome check
     public boolean isPalindrome(int i, int j, String s) {
         while (i < j) {
             if (s.charAt(i) != s.charAt(j)) return false;
@@ -10,33 +10,28 @@ class Solution {
         return true;
     }
 
-    // Recursive function with memoization
-    public int f(int i, int n, String s, int[] dp) {
-        // Base case
-        if (i == n) return 0;
-
-        // Memoization check
-        if (dp[i] != -1) return dp[i];
-
-        int minCost = Integer.MAX_VALUE;
-
-        for (int j = i; j < n; j++) {
-            if (isPalindrome(i, j, s)) {
-                int cost = 1 + f(j + 1, n, s, dp);
-                minCost = Math.min(minCost, cost);
-            }
-        }
-
-        return dp[i] = minCost;
-    }
-
     public int minCut(String s) {
         int n = s.length();
 
-        int[] dp = new int[n];
-        Arrays.fill(dp, -1);
+        // dp[i] = minimum partitions from i → end
+        int[] dp = new int[n + 1];
 
-        // subtract 1 because last partition doesn't need a cut
-        return f(0, n, s, dp) - 1;
+        dp[n] = 0; // base case (same as recursion)
+
+        // fill from right to left
+        for (int i = n - 1; i >= 0; i--) {
+            int minCost = Integer.MAX_VALUE;
+
+            for (int j = i; j < n; j++) {
+                if (isPalindrome(i, j, s)) {
+                    int cost = 1 + dp[j + 1];
+                    minCost = Math.min(minCost, cost);
+                }
+            }
+
+            dp[i] = minCost;
+        }
+
+        return dp[0] - 1; // same adjustment
     }
 }
