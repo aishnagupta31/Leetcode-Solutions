860f6d0d795b2e02b7d8bdc789a3683ab9b3aa39
aishnagupta31 <aishnagupta31@gmail.com>
2026-05-05 16:45:12 -0700

    Time: 183 ms (74.39%), Space: 42.4 MB (95.89%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,73 +1,78 @@
 class Solution {
 
-    public boolean isSafe(char[][] board, int row, int col, char dig) {
-        // Row check
-        for (int j = 0; j < 9; j++) {
-            if (board[row][j] == dig) {
-                return false;
-            }
-        }
-
-        // Column check
-        for (int i = 0; i < 9; i++) {
-            if (board[i][col] == dig) {
-                return false;
-            }
-        }
-
-        // 3x3 Grid check
-        int sr = (row / 3) * 3;
-        int sc = (col / 3) * 3;
-
-        for (int i = sr; i < sr + 3; i++) {
-            for (int j = sc; j < sc + 3; j++) {
-                if (board[i][j] == dig) {
-                    return false;
-                }
-            }
-        }
-
-        return true;
+    public void solveSudoku(char[][] board) {
+        helper(board, 0, 0);
     }
 
     public boolean helper(char[][] board, int row, int col) {
-        // Base case
-        if (row == 9) {
+        // Base case: agar last row cross ho gayi → solved
+        if (row == board.length) {
             return true;
         }
 
-        // Next cell calculation
-        int nextRow = row;
-        int nextCol = col + 1;
+        int nrow = 0;
+        int ncol = 0;
 
-        if (nextCol == 9) {
-            nextRow = row + 1;
-            nextCol = 0;
+        // Next cell calculate karo
+        if (col != board.length - 1) {
+            nrow = row;
+            ncol = col + 1;
+        } else {
+            nrow = row + 1;
+            ncol = 0;
         }
 
-        // Skip filled cells
+        // Agar already filled hai → skip
         if (board[row][col] != '.') {
-            return helper(board, nextRow, nextCol);
-        }
+            return helper(board, nrow, ncol);
+        } else {
+            // Try digits 1 to 9
+            for (int i = 1; i <= 9; i++) {
+                if (isSafe(board, row, col, i)) {
+                    board[row][col] = (char) (i + '0');
 
-        // Try digits 1 to 9
-        for (char dig = '1'; dig <= '9'; dig++) {
-            if (isSafe(board, row, col, dig)) {
-                board[row][col] = dig;
-
-                if (helper(board, nextRow, nextCol)) {
-                    return true;
+                    if (helper(board, nrow, ncol)) {
+                        return true;
+                    } else {
+                        // backtrack
+                        board[row][col] = '.';
+                    }
                 }
-
-                // Backtrack
-                board[row][col] = '.';
             }
         }
 
         return false;
     }
 
-    public void solveSudoku(char[][] board) {
-        helper(board, 0, 0);
+    public boolean isSafe(char[][] board, int row, int col, int num) {
+        char ch = (char) (num + '0');
+
+        // Row check
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == ch) {
+                return false;
+            }
+        }
+
+        // Column check
+        for (int i = 0; i < 9; i++) {
+            if (board[i][col] == ch) {
+                return false;
+            }
+        }
+
+        // 3x3 grid check
+        int sr = (row / 3) * 3;
+        int sc = (col / 3) * 3;
+
+        for (int i = sr; i < sr + 3; i++) {
+            for (int j = sc; j < sc + 3; j++) {
+                if (board[i][j] == ch) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 }
